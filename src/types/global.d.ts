8d51f535772f252df7a41589852bee5d514fe2da
thomthombonard-hash <thomthombonard@gmail.com>
2025-10-29 17:39:02 +0100

    ✅ Build OK - corrections tailwind, nodemailer, types grecaptcha

---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+export {};
+
+declare global {
+  interface Window {
+    grecaptcha: {
+      execute(siteKey: string, options: { action: string }): Promise<string>;
+      ready(cb: () => void): void;
+    };
+  }
+}
