455ffb07cbf4f6e281e6e308c422e99a776e4b5d
thomthombonard-hash <thomthombonard@gmail.com>
2025-10-29 18:16:53 +0100

    Mise à jour globale : layout cookies responsive + autres ajustements pages

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,6 +5,9 @@ declare global {
     grecaptcha: {
       execute(siteKey: string, options: { action: string }): Promise<string>;
       ready(cb: () => void): void;
+      enterprise?: {
+        execute(siteKey: string, options: { action: string }): Promise<string>;
+      };
     };
   }
 }
