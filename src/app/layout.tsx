49f48dc9957570110d83ce1daa4b8823e9c122db
thomthombonard-hash <thomthombonard@gmail.com>
2025-11-20 09:26:21 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,9 +55,32 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://www.google.com" />
 
         <link rel="icon" href="/logo.ico" />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LKB9N8P');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
 
       <body className="flex flex-col min-h-screen text-gray-800 bg-gray-50 relative">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5LKB9N8P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* === BANNIÈRE PROMO SPÉCIALE === */}
         <PromoBanner />
 
